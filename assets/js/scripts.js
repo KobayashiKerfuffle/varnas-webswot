413ef7ca0cb3acf2743d0ceeddf64de398cb7df8
Peter Sadauskas <peter.sadauskas@gmail.com>
2023-10-23 17:28:42 +1030

    Added organisation field for participants.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,65 +1,75 @@
 // scripts.js
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", () => {
+    let nextParticipantIndex = 2;
 
-    // Dynamically adding and removing participants
-    const participantsList = document.getElementById('participants-list');
-
-    // Adding a new participant input group
-    document.getElementById('add-participant').addEventListener('click', function(event) {
+    document.getElementById("add-participant").addEventListener("click", (event) => {
         event.preventDefault();
 
-        const participants = document.querySelectorAll('.participant');
-        const nextParticipantIndex = participants.length + 1;
+        const participantsList = document.getElementById("participants-list");
 
-        const participantDiv = document.createElement('div');
-        participantDiv.className = 'participant';
+        const participantDiv = document.createElement("div");
+        participantDiv.className = "participant";
 
-        const nameLabel = document.createElement('label');
+        // Name
+        const nameLabel = document.createElement("label");
         nameLabel.htmlFor = `participant-name-${nextParticipantIndex}`;
-        nameLabel.innerText = 'Name:';
+        nameLabel.innerText = "Name:";
         participantDiv.appendChild(nameLabel);
 
-        const nameInput = document.createElement('input');
-        nameInput.type = 'text';
+        const nameInput = document.createElement("input");
+        nameInput.type = "text";
         nameInput.id = `participant-name-${nextParticipantIndex}`;
         nameInput.required = true;
         participantDiv.appendChild(nameInput);
 
-        const emailLabel = document.createElement('label');
+        // Email
+        const emailLabel = document.createElement("label");
         emailLabel.htmlFor = `participant-email-${nextParticipantIndex}`;
-        emailLabel.innerText = 'Email:';
+        emailLabel.innerText = "Email:";
         participantDiv.appendChild(emailLabel);
 
-        const emailInput = document.createElement('input');
-        emailInput.type = 'email';
+        const emailInput = document.createElement("input");
+        emailInput.type = "email";
         emailInput.id = `participant-email-${nextParticipantIndex}`;
         emailInput.required = true;
         participantDiv.appendChild(emailInput);
 
-        const roleLabel = document.createElement('label');
+        // Role
+        const roleLabel = document.createElement("label");
         roleLabel.htmlFor = `participant-role-${nextParticipantIndex}`;
-        roleLabel.innerText = 'Role:';
+        roleLabel.innerText = "Role:";
         participantDiv.appendChild(roleLabel);
 
-        const roleInput = document.createElement('input');
-        roleInput.type = 'text';
+        const roleInput = document.createElement("input");
+        roleInput.type = "text";
         roleInput.id = `participant-role-${nextParticipantIndex}`;
         roleInput.required = true;
         participantDiv.appendChild(roleInput);
 
-        participantsList.insertBefore(participantDiv, participantsList.lastChild.previousSibling);
+        // Organization
+        const orgLabel = document.createElement("label");
+        orgLabel.htmlFor = `participant-organization-${nextParticipantIndex}`;
+        orgLabel.innerText = "Organization:";
+        participantDiv.appendChild(orgLabel);
+
+        const orgInput = document.createElement("input");
+        orgInput.type = "text";
+        orgInput.id = `participant-organization-${nextParticipantIndex}`;
+        orgInput.required = true;
+        participantDiv.appendChild(orgInput);
+
+        // Append to participants list and increment the index
+        participantsList.insertBefore(participantDiv, document.getElementById("add-participant"));
+        nextParticipantIndex++;
     });
 
-    // Removing the last participant input group
-    document.getElementById('remove-participant').addEventListener('click', function(event) {
+    document.getElementById("remove-participant").addEventListener("click", (event) => {
         event.preventDefault();
 
-        const participants = document.querySelectorAll('.participant');
+        const participants = document.querySelectorAll(".participant");
         if (participants.length > 1) {
             participants[participants.length - 1].remove();
-        } else {
-            alert('You must have at least one participant.');
         }
     });
 });
